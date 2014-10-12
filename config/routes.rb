4d19866d5850a5bc40b297d982fef746cf622651
Otavio <otamm@hotmail.com>
2014-10-12 08:05:03 -0400

    Configured routes for facebook auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get 'pages/home'
 
   root 'pages#home'
   # You can have the root of your site routed with "root"
