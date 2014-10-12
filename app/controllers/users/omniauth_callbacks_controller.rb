c2609e7b24f37de015ee324e3f7aafc881de0573
Otavio <otamm@hotmail.com>
2014-10-12 04:58:07 -0400

    Configured omniauth controller model to integrate facebook auth with devise

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    if @user.persisted?
+      sign_in_and_redirect(@user)
+    else
+      session["devise.user_attributes"] = @user.attributes
+      redirect_to_new_user_registration_url, notice: "Lmfao, bad bad server, I hope you get stoned"
+    end
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+  
+end
